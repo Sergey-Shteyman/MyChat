241e13e61a3066969eb5d2b2e39cc97bf983486d
Sergey_Shteyman <Shteiman10@gmail.com>
2022-12-02 17:12:33 +0300

    Add Welcome page

---
+++ MyChat/Core/Entities/RobotoFont.swift
@@ -0,0 +1,21 @@
+//
+//  RobotoFont.swift
+//  MyChat
+//
+//  Created by Сергей Штейман on 02.12.2022.
+//
+
+struct RobotoFont {
+    let black = "Roboto-Black"
+    let blackItalic = "Roboto-BlackItalic"
+    let bold = "Roboto-Bold"
+    let boldItalic = "Roboto-BoldItalic"
+    let italic = "Roboto-Italic"
+    let light = "Roboto-Light"
+    let lightItalic = "Roboto-LightItalic"
+    let medium = "Roboto-Medium"
+    let mediumItalic = "Roboto-MediumItalic"
+    let regular = "Roboto-Regular"
+    let thin = "Roboto-Thin"
+    let thinItalic = "Roboto-ThinItalic"
+}
